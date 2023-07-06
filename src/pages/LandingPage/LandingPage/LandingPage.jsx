d743546f48cc4998f19918b1ae79f1997f313cdd
Aminul667 <rahathssn@gmail.com>
2023-07-06 15:39:49 +0600

    added landing page and bg video

--- src/pages/LandingPage/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage/LandingPage.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
+import bgVideo from "../../../assets/videos/background.mp4";
+import './LandingPage.css';
 
 const LandingPage = () => {
-    return (
-        <div>
-            <h1>This is the Landing Page</h1>
-        </div>
-    );
+  return (
+    <div className="container-landingpage">
+      <div className="overlay"></div>
+      <video src={bgVideo} autoPlay loop muted className="container-video"/>
+      <div className="content">
+        <h1>Eclectic</h1>
+        <Link to="/category/all">Read all articles</Link>
+        <Link>Write a new article</Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
