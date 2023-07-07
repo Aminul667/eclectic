f8aa26fbd603fceeab6b96d51feb1fe88319b3ed
Aminul667 <rahathssn@gmail.com>
2023-07-07 13:23:48 +0600

    added new functionality to MyPost component

--- src/pages/MyPosts/MyPosts/MyPosts.jsx
+++ src/pages/MyPosts/MyPosts/MyPosts.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { Link, useLocation } from "react-router-dom";
-import './MyPosts.css';
+import "./MyPosts.css";
 
 const MyPosts = () => {
   const params = useLocation();
@@ -19,22 +19,44 @@ const MyPosts = () => {
     },
   });
 
+  const handleEdit = () => {
+    console.log("Edit Is working");
+  };
+
+  const handleDelete = () => {
+    console.log("Delete is working");
+  };
+
   console.log(isError);
 
   return (
     <div>
       <h2>Articles: {myArticles.length}</h2>
       {isLoading ? <h1>Loading</h1> : ""}
-      {myArticles.map((article, count = 0) => (
-        <div key={article._id} className="container-article">
-          <span>{count + 1}</span>
-          <Link to={`/article/${article._id}`} state={{ id: article._id }}>
-            {article.title}
-          </Link>
-          <button>Edit</button>
-          <button>Delete</button>
-        </div>
-      ))}
+      <div className="container-articles">
+        {myArticles.map((article, count = 0) => (
+          <div key={article._id} className="info-article">
+            <div>
+              <span>{count + 1}.</span>
+              <Link
+                to={`/article/${article._id}`}
+                state={{ id: article._id }}
+                className="link-article"
+              >
+                {article.title}
+              </Link>
+            </div>
+            <div>
+              <button onClick={handleEdit} className="btn-primary btn-margin">
+                Edit
+              </button>
+              <button onClick={handleDelete} className="btn-primary">
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
