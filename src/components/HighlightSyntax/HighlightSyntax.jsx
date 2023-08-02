aaf088caea0e128851aad99833f0aa73631cc6f0
Aminul667 <rahathssn@gmail.com>
2023-08-02 17:59:40 +0600

    fixed code highlighter

--- src/components/HighlightSyntax/HighlightSyntax.jsx
+++ src/components/HighlightSyntax/HighlightSyntax.jsx
@@ -1,14 +1,17 @@
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
-import { atelierCaveDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+// import { atelierCaveDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-// import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const HighlightSyntax = ({ children, language }) => {
+const HighlightSyntax = ({ ...props }) => {
+  const children = props.children;
+  const language = props?.className?.split("-")[1];
+
   return (
     <div>
-      <SyntaxHighlighter
-        language={language ?? null}
-        style={atelierCaveDark}
+      <SyntaxHighlighter 
+        language={language ?? null} 
+        style={atomOneDarkReasonable}
       >
         {children ?? ""}
       </SyntaxHighlighter>
