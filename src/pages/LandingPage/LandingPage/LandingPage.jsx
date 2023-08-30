bd781b06a2de288d1ef735b261bcf645a201afd9
Aminul667 <rahathssn@gmail.com>
2023-08-30 22:50:53 +0600

    added style on Eclectic

--- src/pages/LandingPage/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage/LandingPage.jsx
@@ -8,10 +8,21 @@ const LandingPage = () => {
       <div className="overlay"></div>
       <video src={bgVideo} autoPlay loop muted className="container-video" />
       <div className="content">
-        <h1 className="eclectic">Eclectic</h1>
+        {/* <h1 className="eclectic">Eclectic</h1> */}
+        <div className="patterns">
+          <svg width="100vw" height="100%">
+            <text x="50%" y="60%" textAnchor="middle">
+              Eclectic
+            </text>
+          </svg>
+        </div>
         <div>
-          <Link to="/category/all" className="link-to-article">Read all articles</Link>
-          <Link to="/article/create-article" className="link-to-article">Write a new article</Link>
+          <Link to="/category/all" className="link-to-article">
+            Read all articles
+          </Link>
+          <Link to="/article/create-article" className="link-to-article">
+            Write a new article
+          </Link>
         </div>
       </div>
     </div>
