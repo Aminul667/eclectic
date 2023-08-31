073a2584c05451302e429615862094f90361e5ae
Aminul667 <rahathssn@gmail.com>
2023-08-31 11:27:33 +0600

    added like and bookmark button

--- src/pages/Home/Post/Post.jsx
+++ src/pages/Home/Post/Post.jsx
@@ -1,17 +1,42 @@
 import { Link } from "react-router-dom";
 import "./Post.css";
+import { AiFillLike } from "react-icons/ai";
+import { BsBookmarksFill } from "react-icons/bs";
+import { useState } from "react";
 
 const Post = ({ post }) => {
+  const [like, setLike] = useState(false);
+  const [bookmark, setBookmark] = useState(false);
+  const style = {
+    color: "#71c6dd",
+  };
+
+  const handleLike = () => {
+    setLike(!like);
+  }
+  
   return (
     <div className="post-container">
-      <div className="author-info">
-        <img src={post.photo} className="img-author" />
+      <div className="card-top">
+        <div className="author-info">
+          <img src={post.photo} className="img-author" />
+          <div>
+            <h2 className="author-name">{post.author}</h2>
+            <p className="post-date">{post.date}</p>
+          </div>
+        </div>
         <div>
-          <h2 className="author-name">{post.author}</h2>
-          <p className="post-date">{post.date}</p>
+          <AiFillLike className="like-btn"></AiFillLike>
+          <BsBookmarksFill className="bookmark-btn"></BsBookmarksFill>
         </div>
       </div>
-      <Link to={`/article/${post._id}`} state={{ id: post._id }} className="post-link-title">{post.title}</Link>
+      <Link
+        to={`/article/${post._id}`}
+        state={{ id: post._id }}
+        className="post-link-title"
+      >
+        {post.title}
+      </Link>
     </div>
   );
 };
--- src/pages/LandingPage/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage/LandingPage.jsx
@@ -8,8 +8,7 @@ const LandingPage = () => {
       <div className="overlay"></div>
       <video src={bgVideo} autoPlay loop muted className="container-video" />
       <div className="content">
-        {/* <h1 className="eclectic">Eclectic</h1> */}
-        <div className="patterns">
+        <div>
           <svg width="100vw" height="100%">
             <text x="50%" y="60%" textAnchor="middle">
               Eclectic
